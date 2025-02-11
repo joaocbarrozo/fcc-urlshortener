35751bf7aea8936f94a6c41db9a65c04078cadcb
joaocbarrozo <97959049+joaocbarrozo@users.noreply.github.com>
2025-02-11 13:20:51 -0300

    Update index.js get /api/users/:_id/logs created

--- index.js
+++ index.js
@@ -1,62 +1,111 @@
-require('dotenv').config();
 const express = require('express');
-const cors = require('cors');
 const app = express();
-const dns = require('dns');
+const cors = require('cors');
+require('dotenv').config();
 const crypto = require('crypto');
 
-// Basic Configuration
-const port = process.env.PORT || 3000;
-
 app.use(cors());
+app.use(express.static('public'));
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/views/index.html')
+});
 
-app.use('/public', express.static(`${process.cwd()}/public`));
-
-// Use the express.urlencoded() middleware to parse form data
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
-});
+// Users array
+let users = [];
+// Exercises array
+let exercises = [];
+// Log array
+let log = [];
 
-// Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
-});
-
-const urlMap = new Map();
-
-app.post('/api/shorturl', (req, res) => {
-  const { url } = req.body;
-
-  if (!url) {
-    return res.status(400).json({ error: 'longUrl is required' });
+// Post user endpoint
+app.post('/api/users', (req, res) => {
+  // Get username from form
+  const { username } = req.body;
+  // Check if username already exists
+  if (users.some(user => user.username === username)){
+    res.json({'error': 'User already exists'});
+  // Create a new user
+  } else {
+    // Generate a user id
+    const userId = `${crypto.randomBytes(8).toString('hex')}`;
+    // Create a user object
+    const newUser = {'username': username, '_id': userId};
+    // Add new user to users array
+    users.push(newUser);
+    // Add user to log
+    const newLog = {
+      'username': username,
+      'count': 0,
+      '_id': userId,
+      'log':[]
+    };
+    log.push(newLog);
+    // Response JSON with new user added
+    res.json(newUser);
   }
 
-  // Verify the submitted URL
-  dns.lookup(new URL(url).hostname, (err) => {
-    if (err) {
-      return res.json({ error: 'invalid URL' });
-    }
-
-    const shortUrl = `${crypto.randomBytes(3).toString('hex')}`;
-    urlMap.set(shortUrl, url);
-    res.json({ 'original_url': url, 'short_url': shortUrl });
-  });
-
-  app.get('/api/shorturl/:shorturl', (req, res) => {
-    const { shorturl } = req.params;
-    const longUrl = urlMap.get(shorturl);
-
-    if (longUrl) {
-      res.redirect(longUrl)
-    } else {
-      res.json({ error: 'Short URL not found' });
-    }
-  });
-
+});
+// Get users endpoint
+app.get('/api/users', (req, res) => {
+  res.json(users);
 });
 
-app.listen(port, function() {
-  console.log(`Listening on port ${port}`);
+// Post exercises endpoint
+app.post('/api/users/:_id/exercises', (req, res) => {
+  // Get id from parameters
+  const { _id } = req.params;
+  // Find user wit the input id
+  const username = users.find(user => user._id === _id).username;
+  if (username) {
+    const { description, duration, date } = req.body;
+    // Cast duration to INT
+    const exerciseDuration = parseInt(duration);
+    // Format date
+    const exerciseDate = date ? new Date(date).toDateString(): new Date().toDateString();
+    // Create object newExercise
+    const newExercise = { 
+      'username': username,
+      'description': description, 
+      'duration': exerciseDuration, 
+      'date': exerciseDate,
+      'id': _id
+    };
+    // Add newExercise to exercises array
+    exercises.push(newExercise);
+    // Create object newLog
+    const newLog = {
+      'description': description, 
+      'duration': exerciseDuration, 
+      'date': exerciseDate
+    }
+    // Find the user in log array
+    const userLog = log.find(user => user._id === _id);
+    // Update user log and count
+    userLog.log.push(newLog);
+    userLog.count++;
+    // Response JSON with the user and exercise added
+    res.json({
+      '_id': _id,
+      'username': username,
+      'date': exerciseDate,
+      'duration': exerciseDuration,
+      'description': description
+    })
+  } else {
+    res.json({'error': 'user not found'});
+  }
+  
 });
+
+// Get /api/users/log endpoint
+app.get('/api/users/:_id/logs', (req, res) => {
+  const { _id } = req.params;
+  const userLog = log.find(user => user._id === _id);
+  res.json( userLog );
+});
+
+const listener = app.listen(process.env.PORT || 3000, () => {
+  console.log('Your app is listening on port ' + listener.address().port)
+})
